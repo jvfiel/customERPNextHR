77d7a6976548f4bfb0cb82b77a388b3ac50ac7d6
John Vincent Fiel <johnvincentfiel@gmail.com>
2018-11-11 12:28:06 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
 import re, ast
+
 
 # get version from __version__ variable in payslip_app/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
